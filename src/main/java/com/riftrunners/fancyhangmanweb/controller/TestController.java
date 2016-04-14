dd8e1954434b21586bf38c484d5a9679dd9e5709
Guilherme Matuella <guilherme1matuella@gmail.com>
2016-04-14 11:50:51 -0300

    Teste inicial funcionando com o servidor WildFly

---
+++ src/main/java/com/riftrunners/fancyhangmanweb/controller/TestController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.riftrunners.fancyhangmanweb.controller;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author 631420063
+ */
+@ManagedBean(eager = true)
+@ApplicationScoped
+public class TestController {
+    
+    String nome;
+    /**
+     * Creates a new instance of TestController
+     */
+    public TestController() {
+        this.nome = "Teste!";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
